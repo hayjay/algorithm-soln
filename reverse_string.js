4afc322b5f28af4263456ce08413e6e7234f9416
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-12-11 18:03:02 +0100

    reverse algorithm

---
+++ reverse_string.js
@@ -0,0 +1,35 @@
+let reverseString = (string_example) => {
+    //split the string the user input first
+    // so that each letter of the string would represent an array
+    //we are spliting so that we can use each element of the string for something else like : reversal or join
+    let string_arr = string_example.split("");
+    let string_reverse = '';
+    string_arr.map((el) => {
+        if(!isNaN())
+    });
+    let string_reverse = string_arr.reverse();
+    let result = string_reverse.join("");
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // OR
+    // let result = string_example.split("").reverse().join("");
+    console.log(result);
+}
+
+reverseString('m01ariam');
