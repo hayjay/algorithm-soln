08e4fd046899f41b506567b62ef39dc44a412a97
hayjay <new@MacBook-Pro-95.local>
2018-12-13 02:22:39 +0100

    solved reverse string

--- reverse_string.js
+++ reverse_string.js
@@ -3,33 +3,27 @@ let reverseString = (string_example) => {
     // so that each letter of the string would represent an array
     //we are spliting so that we can use each element of the string for something else like : reversal or join
     let string_arr = string_example.split("");
-    let string_reverse = '';
-    string_arr.map((el) => {
-        if(!isNaN())
-    });
-    let string_reverse = string_arr.reverse();
-    let result = string_reverse.join("");
-    
+    let rev = '';
+    if(string_arr){
+        string_arr.forEach(function (each_string, its_index) {
+            if(!isNaN(each_string)){//if current index is a number
+                // if it is, leave the number in same position
+                each_string = each_string + ' is not a number ';
+            }else{
+                is_string = each_string + ' is not a number ';
 
+                string_arr.reverse();
+            }
+            console.log(its_index + 'is a string');
+            console.log(each_string + ' Value');
+        });
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let result = string_arr.join(" ");
 
     // OR
     // let result = string_example.split("").reverse().join("");
     console.log(result);
 }
 
-reverseString('m01ariam');
+reverseString('str1ngw1thnumb3rs');
